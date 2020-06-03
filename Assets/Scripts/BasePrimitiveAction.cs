ca1b20f7ba67e381a27ea93d45629d44a7a8458a
joselu <JosBY64@gmail.com>
2020-06-03 17:08:50 +0200

    Initial commit

---
+++ Assets/Scripts/BasePrimitiveAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pada1.BBCore;
+using Pada1.BBCore.Tasks;
+using Pada1.BBCore.Framework;
+
+
+[Action("ESCAV/SetEnemyChasing")]
+public class SetEnemyChasing : BasePrimitiveAction
+{
+
+    [InParam("value")]
+    public bool value;
+
+    public override TaskStatus OnUpdate()
+    {
+        GlobalBlackboard.EnemyChasing = value;
+        return TaskStatus.COMPLETED;
+    }
+   
+
+}
---
+++ Assets/Scripts/IsEnemyChasing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pada1.BBCore;
+using Pada1.BBCore.Tasks;
+using Pada1.BBCore.Framework;
+
+[Condition("Escav/IsEnemyChasing")]
+public class IsEnemyChasing : ConditionBase
+{
+
+    public override bool Check()
+    {
+        return GlobalBlackboard.EnemyChasing;
+    }
+
+
+
+}
+
